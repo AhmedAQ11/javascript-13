a2f47b780ae0a2224bd1b72221739412f48a1687
AhmedAQ11 <m.ahmedAQ.786@gmail.com>
2021-12-05 23:07:10 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,85 @@
+   ///// CHAPTER 58-67/////
+
+
+   /////    Q1 ////////
+
+
+
+
+// var mainContent = document.getElementById("main-content")
+ 
+// var firstCon = document.getElementById("main-content")
+
+// firstCon.children
+
+// console.log(firstCon)
+// document.write(firstCon)
+// var className = document.getElementsByClassName("render")
+
+// console.log( className)
+// document.write(className)
+
+// var firstName = document.getElementById("first-name")
+
+// console.log( firstName.value = "Ahmed")
+
+// var lastName = document.getElementById("last-name")
+// console.log(lastName)
+// lastName.value = "Muhammed"
+
+// var emailAdd = document.getElementById("email")
+
+// console.log(emailAdd)
+
+// emailAdd.value = "ahmed123@gmail.com"
+
+
+
+
+
+// /////// Q2 ///////
+
+
+// var formContent = document.getElementById("form-content")
+
+// console.log(formContent.nodeType)
+// document.write(formContent.nodeType)
+
+// var lastName = document.getElementById("lastName")
+
+
+// console.log(lastName.nodeType)
+// document.write(lastName.nodeType )
+
+// var childNode = lastName.childNodes
+
+// console.log(childNode)
+
+
+// var mainContent = document.getElementById("main-content")
+
+
+// document.write (mainContent.firstElementChild)
+// console.log(mainContent.firstElementChild)
+// document.write(mainContent.lastElementChild)
+// console.log(mainContent.lastElementChild)
+
+
+// var lastName = document.getElementById("lastName")
+
+
+
+// console.log(lastName.nextElementSibling)
+// document.write(lastName.nextElementSibling)
+// console.log(lastName.previousElementSibling)
+// document.write(lastName.previousElementSibling)
+
+
+// var emailAdd = document.getElementById("email")
+
+
+
+// console.log(emailAdd.parentNode)
+// document.write(emailAdd.parentNode)
+// console.log(emailAdd.nodeType)
+// document.write(emailAdd.nodeType)
